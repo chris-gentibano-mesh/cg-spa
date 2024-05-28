bfc776bc743600d16799d4c933e959f8d8ed6d57
Chris Gentibano <christopher.gentibano@meshconnect.com>
2024-05-28 16:23:24 -0700

    restructured + edited files to use next.js

---
+++ mesh-web-app/src/components/BrokerConnectButton.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import { createLink } from '@meshconnect/web-link-sdk';
+import { MESH_CLIENTID, MESH_APIKEY, MESH_URL, MESH_USERID } from '../utility/config';
+
+const BrokerConnectButton = ({ authLink, integrationId }) => {
+  const [linkConnection, setLinkConnection] = useState(null);
+  const [linkToken, setLinkToken] = useState(null);
+
+  useEffect(() => {
+    const link = createLink({
+      clientId: MESH_CLIENTID,
+      onIntegrationConnected: (data) => {
+
+        console.log('Integration connected:', data);
+      },
+      onExit: (error) => {
+        if (error) {
+          // Handle error
+          console.error('Link exited with error:', error);
+        } else {
+          // Handle successful exit without error
+          console.log('Link exited successfully');
+        }
+      }
+    });
+    setLinkConnection(link);
+  }, []);
+
+  useEffect(() => {
+    if (linkConnection && authLink) {
+      linkConnection.openLink(linkToken);
+    }
+  }, [linkConnection, authLink, linkToken]);
+  
+  const fetchLinkToken = async () => {
+    try {
+      const response = await fetch(MESH_URL + '/api/v1/linktoken', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/*+json',
+          'x-client-id': MESH_CLIENTID,
+          'x-client-secret': MESH_APIKEY, 
+        },
+        body: JSON.stringify({
+          userId: MESH_USERID,
+          integrationId: integrationId
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to obtain linkToken');
+      }
+
+      const data = await response.json();
+      console.log(data)
+      setLinkToken(data.content.linkToken);
+    } catch (error) {
+      console.error('Error obtaining link token:', error);
+    }
+  };
+
+  const handleButtonClick = async () => {
+    await fetchLinkToken(); // Fetch the link token when the button is clicked
+    console.log("fetch done")
+    console.log(linkConnection)
+    console.log("link token:")
+    console.log(linkToken)
+    if (linkConnection && linkToken) {
+      console.log("opening link")
+      linkConnection.openLink(linkToken);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleButtonClick} style={styles.button}>
+        Open Specified Broker
+      </button>
+    </div>
+  );
+};
+
+const styles = {
+    button: {
+      padding: '10px 20px',
+      fontSize: '1rem',
+      borderRadius: '5px',
+      border: 'none',
+      backgroundColor: '#007bff',
+      color: 'white',
+      cursor: 'pointer'
+    }
+  };
+  
+export default BrokerConnectButton;
--- mesh-web-app/src/utility/config.js
+++ mesh-web-app/src/utility/config.js
@@ -1,4 +1,4 @@
-export const MESH_CLIENTID = process.env.REACT_APP_CLIENT_ID
-export const MESH_APIKEY = process.env.REACT_APP_CLIENT_SECRET
-export const MESH_USERID = process.env.REACT_APP_USER_ID
-export const MESH_URL = process.env.REACT_APP_URL
+export const MESH_CLIENTID = process.env.MESH_CLIENTID;
+export const MESH_APIKEY = process.env.MESH_APIKEY;
+export const MESH_USERID = process.env.MESH_USERID;
+export const MESH_URL = process.env.MESH_URL;
